8360f907383166262f9b04216b348727c1d17a0d
Ultramente <165812620+PedroGabriel35@users.noreply.github.com>
2024-09-27 12:20:46 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é PSG kk'
+)
+criaCartao(
+    'Programação',
+    "O que é uma função?",
+    'Uma função é um bloco de código que executa alguma tarefa, semelhante a um CLT',
+)
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz OI em Inglês?',
+    'OI em Inglês é HI (RAIos)'
+)
